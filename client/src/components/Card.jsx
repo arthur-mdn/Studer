e44dbd8bc8dce5034d38a89e29f5975cd73c0fdc
Arthur Mondon <arthur@mondon.pro>
2024-05-14 22:14:37 +0200

    update car buttons

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-import {FaArrowRight, FaArrowRightLong, FaComment, FaInfo, FaMessage} from "react-icons/fa6";
+import {FaArrowRightLong, FaComment, FaInfo} from "react-icons/fa6";
 import config from '../config';
 
-function Card({realization, onView, onAddToChat}) {
+function Card({ realization, onView, onAddToChat }) {
+    const handleCardClick = () => {
+        onView();
+    };
+
+    const handleChatClick = (e) => {
+        e.stopPropagation(); // Prevent the card's click event from firing
+        onAddToChat();
+    };
+
     return (
-        <div className="realization-card">
-            <img src={`${config.serverUrl}/${realization.firstImage}`} alt={realization.title}/>
+        <div className="realization-card" onClick={handleCardClick} style={{ cursor: "pointer" }}>
+            <img src={`${config.serverUrl}/${realization.firstImage}`} alt={realization.title} />
+            <div className={"fc ai-c jc-c o0-5"} style={{position:"absolute", right:"1rem", top:"1rem", border:'1px solid black', borderRadius:'5rem',width:"1.6rem", height:"1.6rem"}}>
+                <FaInfo style={{marginBottom:"0.2rem"}}/>
+            </div>
             <div className={"shadow"}></div>
             <div className="realization-info">
                 <h2>{realization.title}</h2>
                 <p>{realization.description}</p>
                 <div className={"realization-buttons"}>
-                    <button type={"button"} className={"see-more"} onClick={
-                        onView
-                    }>En savoir plus <FaArrowRightLong/></button>
-                    <button className="add-to-chat" onClick={onAddToChat}><FaComment/></button>
+                    <button type="button" className="see-more" onClick={handleChatClick}>
+                        J'ai une question <FaArrowRightLong />
+                    </button>
                 </div>
             </div>
-
         </div>
     );
 }
