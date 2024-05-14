84dee7be05c9e5aa9ab2bf0072ee38bf8ca55de0
Arthur Mondon <arthur@mondon.pro>
2024-05-14 22:27:38 +0200

    fix info icon on card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -15,8 +15,8 @@ function Card({ realization, onView, onAddToChat }) {
     return (
         <div className="realization-card" onClick={handleCardClick} style={{ cursor: "pointer" }}>
             <img src={`${config.serverUrl}/${realization.firstImage}`} alt={realization.title} />
-            <div className={"fc ai-c jc-c o0-5"} style={{position:"absolute", right:"1rem", top:"1rem", border:'1px solid black', borderRadius:'5rem',width:"1.6rem", height:"1.6rem"}}>
-                <FaInfo style={{marginBottom:"0.2rem"}}/>
+            <div className={"fc ai-c jc-c o0-5"} style={{position:"absolute", right:"1rem", top:"1rem", border:'1px solid white',zIndex:'4', borderRadius:'5rem',width:"1.6rem", height:"1.6rem"}}>
+                <FaInfo style={{marginBottom:"0.2rem", color:"white"}}/>
             </div>
             <div className={"shadow"}></div>
             <div className="realization-info">
