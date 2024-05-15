8bb5ab070122761ac433fbe7fdafb2dbc2be21a3
Arthur Mondon <arthur@mondon.pro>
2024-05-15 10:23:24 +0200

    add shadow to info icon

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -15,7 +15,7 @@ function Card({ realization, onView, onAddToChat }) {
     return (
         <div className="realization-card" onClick={handleCardClick} style={{ cursor: "pointer" }}>
             <img src={`${config.serverUrl}/${realization.firstImage}`} alt={realization.title} />
-            <div className={"fc ai-c jc-c o0-5"} style={{position:"absolute", right:"1rem", top:"1rem", border:'1px solid white',zIndex:'4', borderRadius:'5rem',width:"1.6rem", height:"1.6rem"}}>
+            <div className={"fc ai-c jc-c o0-5"} style={{position:"absolute", right:"1rem", top:"1rem", border:'1px solid white',zIndex:'4', borderRadius:'5rem',width:"1.6rem", height:"1.6rem", boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>
                 <FaInfo style={{marginBottom:"0.2rem", color:"white"}}/>
             </div>
             <div className={"shadow"}></div>
